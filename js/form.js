36fcbaf5f72a9e7591f31cdd79f90ce98e4e909d
mrbassx <abassiddrisu9222@gmail.com>
2025-10-25 17:34:24 +0000

    const element = document.createElement("h1");       element.innerHTML = ""

--- js/form.js
+++ js/form.js
@@ -1,7 +1,11 @@
 
 
+
 const EvenOrOdd = document.getElementById("EvenOROdd")
 const send = document.getElementById("send")
+const display = document.getElementById("display")
+
+console.log(display)
 
 
 
@@ -9,6 +13,16 @@ send.onclick = function Test(e) {
     
     e.preventDefault();
 
-    alert(EvenOrOdd.value)
+  
+
+    let Number =parseFloat(EvenOrOdd.value);
+    let IsEven = Number%2 === 0
+     IsEven ? display.innerHTML =("Yeah,Is Even") :display.innerHTML = ("Nope, Is Odd");
+
+     
+
+
+
+
 }
 
