56630a101d6686fe6e907601b1c67e1cb7ba283d
mrbassx <Abassiddrisu9222@gmail.com>
2025-11-23 16:52:21 +0000

    updated..........................

---
+++ js/scripter.js
@@ -0,0 +1,30 @@
+// let l = prompt("Input Value Of L");
+l = 12
+let w = 10;
+let z = 200
+let y = 100
+let k = 2 * (w) + 3 * (y) + z + 4 * (l)
+
+
+
+
+console.log(k)
+
+document.getElementById("fucker").innerHTML = "Mr.A bass"
+let containerA = '123'
+const converted = parseFloat(containerA)
+console.log(typeof (converted))
+
+
+let first = 90;
+let second = 200;
+
+let final = second + first;
+let final2 = "The sum of  " + first + " + " + second + " =  " + (final);
+console.log(final2);
+
+
+let king = 900 > 100;
+console.log(!king)
+!king ? console.log(first + " Number is Small") : console.log(first + " Number Is Great")
+
