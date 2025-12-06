bda859d941414c18eafd04068de1a0cdc75d44ef
mrbassx <abassiddrisu9222@gmail.com>
2025-12-06 17:04:32 +0000

    updated...

--- js/loops.js
+++ js/loops.js
@@ -3,24 +3,49 @@
 // For Loop
 
 
-let student = ["Gideon","Godfred","Mr","Mrs"]
+let student = ["Gideon","Godfred","Mr","Mrs","Another"]
 
+console.log(student.length)
 // console.log(student[2])
 // console.log(student[3])
 // console.log(student[0])
 // console.log(student[1])
+if (student.length) {
 
-for(let i =0;i < 4; i = i + 1 ){
+    for(let i =0;i < student.length; i = i + 1 ){
 
+ 
     console.log(student[i]);
+   
+
+}
+
+}else{
+    console.log("No Record Found")
 }
 
 
 
 //While Loop
 
+let i = 0;
+while(i < student.length){
+
+    console.log(student[i]);
+
+    i = i +1;
+}
 
 
 
 // Do...While Loop
+var input;
+do{
+
+     input= prompt("Hey!! Whats your Name?")
+
+     alert("Yeah, Your Name is  " + input);
+     
+
+}while(input != ('x'));
 
