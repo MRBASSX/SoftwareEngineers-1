0422b9ecf6ec27dfa7babc83202504eca1ad8627
mrbassx <abassiddrisu9222@gmail.com>
2025-12-06 16:14:16 +0000

    updated...

--- js/loops.js
+++ js/loops.js
@@ -2,9 +2,17 @@
 
 // For Loop
 
-for(let i =20;i >= -10; i = i - 1 ){
 
-    console.log(i);
+let student = ["Gideon","Godfred","Mr","Mrs"]
+
+// console.log(student[2])
+// console.log(student[3])
+// console.log(student[0])
+// console.log(student[1])
+
+for(let i =0;i < 4; i = i + 1 ){
+
+    console.log(student[i]);
 }
 
 
